46f18361b9f4f4b81e4366ee91ebb48548548883
tanvir <tzather@gmail.com>
2023-08-06 15:26:45 -0600

    fix broken guild

--- src/controls/card-mat/card-mat.component.ts
+++ src/controls/card-mat/card-mat.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'card-mat',
   templateUrl: './card-mat.component.html',
   styleUrls: ['./card-mat.component.scss'],
-  imports: [MatCardModule],
 })
 export class CardMatComponent extends CardComponent {
 }
